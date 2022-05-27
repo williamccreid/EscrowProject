37c5cb214fab5d6e73b507b87526e5dd0cf6581d
williamccreid <williamccreid@gmail.com>
2022-05-26 23:55:07 -0400

    changed value from wei to ether

--- app/index.js
+++ app/index.js
@@ -7,7 +7,7 @@ let contracts = 0;
 async function newContract() {
   const beneficiary = document.getElementById("beneficiary").value;
   const arbiter = document.getElementById("arbiter").value;
-  const value = ethers.BigNumber.from(document.getElementById("wei").value);
+  const value = ethers.utils.parseEthers(document.getElementById("wei").value);
   const contract = await deploy(arbiter, beneficiary, value);
   addContract(++contracts, contract, arbiter, beneficiary, value);
 }
